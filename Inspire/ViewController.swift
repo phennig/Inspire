a77a4c7c3b00f4df5ef7da78132b971a142bb8a6
phennig <paul.hennig@d214.org>
2019-11-13 09:46:28 -0600

    Initial Commit

---
+++ Inspire/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Inspire
+//
+//  Created by period2 on 11/12/19.
+//  Copyright © 2019 period2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
