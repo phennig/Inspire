c27cb173885b948de5738422d36f75cf911e04cb
phennig <paul.hennig@d214.org>
2019-11-13 09:55:01 -0600

    What up boys

--- Inspire/ViewController.swift
+++ Inspire/ViewController.swift
@@ -12,7 +12,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+//control click on folder and create remote
+        
     }
 
 
